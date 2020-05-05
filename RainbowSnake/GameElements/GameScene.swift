b7465efeda20a3899b107065c28231ad93b6a430
Theo Carrive <theo@cheerz.com>
2020-05-05 20:14:52 +0200

    Change background color after eating apple

--- RainbowSnake/GameElements/GameScene.swift
+++ RainbowSnake/GameElements/GameScene.swift
@@ -4,21 +4,30 @@ import GameplayKit
 class GameScene: SKScene {
 
     let snake = Snake()
-
+    let background: SKSpriteNode
+    
     var score: Int = 0
     var onFailure: ((Int) -> Void)? = nil
 
     init(size: CGSize, onFailure: @escaping (Int) -> Void) {
         self.onFailure = onFailure
+        self.background = SKSpriteNode(color: UIColor(named: "GameBackground")!, size: size)
         super.init(size: size)
-        backgroundColor = UIColor(named: "GameBackground")!
     }
-
+    
     required init(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
+    
+    func changeBackgroundColor() {
+        let randomHue = CGFloat.random(in: 0...1.0)
+        let newColor = UIColor(hue: randomHue, saturation: 0.9, brightness: 0.3, alpha: 1.0)
+        let colorize = SKAction.colorize(with: newColor, colorBlendFactor: 0.5, duration: 0.5)
+        background.run(colorize)
+    }
 
     func start() {
+        addChild(background)
         anchorPoint = CGPoint(x: 0.5, y: 0.5)
         
         physicsWorld.gravity = .zero
@@ -61,6 +70,7 @@ extension GameScene: SKPhysicsContactDelegate {
         Apple.spawn(scene: self)
         snake.increaseLength(scene: self)
         snake.increaseSpeed()
+        changeBackgroundColor()
     }
 
     func didBegin(_ contact: SKPhysicsContact) {
