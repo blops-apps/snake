7a7a801d3ca128de8a2e1dba02b855a161cff659
Theo C <theo@cheerz.com>
2020-07-18 11:48:32 +0200

    Easier apple positionning

--- RainbowSnake/GameElements/Apple.swift
+++ RainbowSnake/GameElements/Apple.swift
@@ -8,8 +8,8 @@ class Apple {
     static func spawn(scene: SKScene, color: UIColor) {
         let node = SKSpriteNode(color: color, size: appleSize)
 
-        let x = CGFloat.random(in: 0...scene.size.width) / 1.2 - scene.size.width / 2
-        let y = CGFloat.random(in: 0...scene.size.height) / 1.2 - scene.size.height / 2
+        let x = CGFloat.random(in: 0...scene.size.width) / 1.3 - scene.size.width / 2
+        let y = CGFloat.random(in: 0...scene.size.height) / 1.3 - scene.size.height / 2
 
         node.physicsBody = GamePhysicsFactory.rectangle(size: appleSize, category: .apple, testContact: .snake)
 
